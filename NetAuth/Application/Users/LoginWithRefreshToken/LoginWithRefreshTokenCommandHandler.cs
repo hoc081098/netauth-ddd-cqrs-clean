f18994ec200744814b8e130e9923d4316ad62607
hoc081098@gmail.com <hoc081098@gmail.com>
2025-11-30 18:03:46 +0700

    Add LoginWithRefreshTokenCommandHandler to handle refresh token login

---
+++ NetAuth/Application/Users/LoginWithRefreshToken/LoginWithRefreshTokenCommandHandler.cs
@@ -0,0 +1,56 @@
+using LanguageExt;
+using Microsoft.Extensions.Options;
+using NetAuth.Application.Abstractions.Authentication;
+using NetAuth.Application.Abstractions.Common;
+using NetAuth.Application.Abstractions.Data;
+using NetAuth.Application.Abstractions.Messaging;
+using NetAuth.Domain.Core.Primitives;
+using NetAuth.Domain.Users;
+using NetAuth.Infrastructure.Authentication;
+
+namespace NetAuth.Application.Users.LoginWithRefreshToken;
+
+internal sealed class LoginWithRefreshTokenCommandHandler(
+    IRefreshTokenRepository refreshTokenRepository,
+    IUserRepository userRepository,
+    IJwtProvider jwtProvider,
+    IRefreshTokenGenerator refreshTokenGenerator,
+    IOptions<JwtConfig> jwtConfigOptions,
+    IClock clock,
+    IUnitOfWork unitOfWork) :
+    ICommandHandler<LoginWithRefreshTokenCommand, LoginWithRefreshTokenResult>
+{
+    public async Task<Either<DomainError, LoginWithRefreshTokenResult>> Handle(
+        LoginWithRefreshTokenCommand command,
+        CancellationToken cancellationToken)
+    {
+        // 1. Find the refresh token
+        var storedRefreshToken = await refreshTokenRepository.GetByTokenAsync(
+            command.RefreshToken,
+            cancellationToken);
+
+        // 2. Check if token exists
+        if (storedRefreshToken is null)
+        {
+            return UsersDomainErrors.RefreshToken.Invalid;
+        }
+
+        // 3. Check if token is expired
+        if (storedRefreshToken.IsExpired(clock.UtcNow))
+        {
+            return UsersDomainErrors.RefreshToken.Expired;
+        }
+
+        // 4. Generate new access token and new refresh token
+        var accessToken = jwtProvider.Create(storedRefreshToken.User);
+        storedRefreshToken.UpdateToken(
+            token: refreshTokenGenerator.GenerateToken(),
+            expiresOnUtc: clock.UtcNow.Add(jwtConfigOptions.Value.RefreshTokenExpiration));
+        await refreshTokenRepository.DeleteExpiredByUserIdAsync(storedRefreshToken.UserId, clock.UtcNow, cancellationToken);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return new LoginWithRefreshTokenResult(
+            AccessToken: accessToken,
+            RefreshToken: storedRefreshToken.Token);
+    }
+}
