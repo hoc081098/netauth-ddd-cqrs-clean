5fc381c40226e8c4b95c0566cb1df60e46889a27
hoc081098@gmail.com <hoc081098@gmail.com>
2025-12-06 20:11:39 +0700

    Change return type of GetByUserIdAsync method from IReadOnlyCollection to IReadOnlyList

--- NetAuth/Domain/TodoItems/ITodoItemRepository.cs
+++ NetAuth/Domain/TodoItems/ITodoItemRepository.cs
@@ -4,7 +4,7 @@ public interface ITodoItemRepository
 {
     Task<TodoItem?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
 
-    Task<IReadOnlyCollection<TodoItem>> GetByUserIdAsync(Guid userId, CancellationToken cancellationToken = default);
+    Task<IReadOnlyList<TodoItem>> GetByUserIdAsync(Guid userId, CancellationToken cancellationToken = default);
 
     void Insert(TodoItem todoItem);
 
