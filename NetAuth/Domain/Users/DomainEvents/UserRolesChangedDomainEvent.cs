ee64d34f7f01f30de5fb1740a08e677a93fbfccd
hoc081098@gmail.com <hoc081098@gmail.com>
2025-12-15 01:22:30 +0700

    Use HashSet for OldRoleIds and NewRoleIds in UserRolesChangedDomainEvent to prevent deserialization exceptions

--- NetAuth/Domain/Users/DomainEvents/UserRolesChangedDomainEvent.cs
+++ NetAuth/Domain/Users/DomainEvents/UserRolesChangedDomainEvent.cs
@@ -4,5 +4,8 @@ namespace NetAuth.Domain.Users.DomainEvents;
 
 public sealed record UserRolesChangedDomainEvent(
     Guid UserId,
+    // Must use HashSet here to avoid the exception when deserializing:
+    //     System.NotSupportedException: The collection type 'System.Collections.Generic.IReadOnlySet`1[NetAuth.Domain.Users.RoleId]'
+    //     is abstract, an interface, or is read only, and could not be instantiated and populated.
     HashSet<RoleId> OldRoleIds,
     HashSet<RoleId> NewRoleIds) : IDomainEvent;
