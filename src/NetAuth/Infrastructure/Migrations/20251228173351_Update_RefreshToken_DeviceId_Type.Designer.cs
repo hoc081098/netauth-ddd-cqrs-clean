26cad2e99b4cc97eb1463f176bb07addd3200815
hoc081098@gmail.com <hoc081098@gmail.com>
2025-12-29 00:44:56 +0700

    Update DeviceId type from string to Guid in refresh tokens

--- src/NetAuth/Infrastructure/Migrations/20251228173351_Update_RefreshToken_DeviceId_Type.Designer.cs
+++ src/NetAuth/Infrastructure/Migrations/20251228173351_Update_RefreshToken_DeviceId_Type.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NetAuth.Infrastructure;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace NetAuth.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251228173351_Update_RefreshToken_DeviceId_Type")]
+    partial class Update_RefreshToken_DeviceId_Type
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/NetAuth/Infrastructure/Migrations/20251228173351_Update_RefreshToken_DeviceId_Type.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NetAuth.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update_RefreshToken_DeviceId_Type : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "device_id",
+                table: "refresh_tokens",
+                type: "uuid using device_id::uuid",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(256)",
+                oldMaxLength: 256);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "device_id",
+                table: "refresh_tokens",
+                type: "character varying(256)",
+                maxLength: 256,
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+        }
+    }
+}
